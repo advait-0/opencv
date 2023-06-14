a57483d7be41f934721f3aae6d4d95df53747195
Advait Dhamorikar <advaitdhamorikar@gmail.com>
2023-06-15 00:17:03 +0100

    update cap_libcamera.cpp

--- modules/videoio/src/cap_libcamera.cpp
+++ modules/videoio/src/cap_libcamera.cpp
@@ -3,36 +3,58 @@
 #include <memory>
 
 #include <libcamera/libcamera.h>
-#include <opencv2/core/utils/logger.hpp>
-#include <opencv2/core/utils/filesystem.hpp>
-
 
 #define TIMEOUT_SEC 3
 
 using namespace libcamera;
 static std::shared_ptr<Camera> camera;
 
+std::string cameraName(Camera* camera)
+{
+    const ControlList& props = camera->properties();
+    std::string name;
+
+    const auto& location = props.get(properties::Location);
+    if (location) {
+        switch (*location) {
+            case properties::CameraLocationFront:
+                name = "Internal front camera";
+                break;
+            case properties::CameraLocationBack:
+                name = "Internal back camera";
+                break;
+            case properties::CameraLocationExternal:
+                name = "External camera";
+                const auto& model = props.get(properties::Model);
+                if (model)
+                    name += " '" + *model + "'";
+                break;
+        }
+    }
+
+    name += " (" + camera->id() + ")";
+
+    return name;
+}
+
 int main()
 {
-	
-	
-	std::unique_ptr<CameraManager> cm = std::make_unique<CameraManager>();
-	cm->start();
+    std::unique_ptr<CameraManager> cm = std::make_unique<CameraManager>();
+    cm->start();
 
-	
-	for (auto const &camera : cm->cameras())
-		std::cout << " - " << cameraName(camera.get()) << std::endl;
+    for (const auto& camera : cm->cameras())
+        std::cout << " - " << cameraName(camera.get()) << std::endl;
 
+    if (cm->cameras().empty()) {
+        std::cout << "No cameras were identified on the system" << std::endl;
+        cm->stop();
+        return EXIT_FAILURE;
+    }
 
-	if (cm->cameras().empty()) {
-		std::cout << "No cameras were identified on the system"<< std::endl;
-		cm->stop();
-		return EXIT_FAILURE;
-	}
+    std::string cameraId = cm->cameras()[0]->id();
+    camera = cm->get(cameraId);
+    std::cout << "Cameras available yayy" << cameraName(camera.get()) << std::endl;
 
-	std::string cameraId = cm->cameras()[0]->id();
-	camera = cm->get(cameraId);
-	std::cout<<"Cameras available yayy";
-	return cv::CAP_LIBCAMERA;
-
+    return 0;
 }
+

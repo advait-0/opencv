60f9d2159746b43c1a14ee6270c6adce34473720
Umang Jain <umang.jain@ideasonboard.com>
2023-06-15 11:36:19 +0530

    Remove cameraName to solve compile errors
    Signed-off-by: Umang Jain <umang.jain@ideasonboard.com>

--- modules/videoio/src/cap_libcamera.cpp
+++ modules/videoio/src/cap_libcamera.cpp
@@ -7,43 +7,11 @@
 #define TIMEOUT_SEC 3
 
 using namespace libcamera;
-static std::shared_ptr<Camera> camera;
-
-std::string cameraName(Camera* camera)
-{
-    const ControlList& props = camera->properties();
-    std::string name;
-
-    const auto& location = props.get(properties::Location);
-    if (location) {
-        switch (*location) {
-            case properties::CameraLocationFront:
-                name = "Internal front camera";
-                break;
-            case properties::CameraLocationBack:
-                name = "Internal back camera";
-                break;
-            case properties::CameraLocationExternal:
-                name = "External camera";
-                const auto& model = props.get(properties::Model);
-                if (model)
-                    name += " '" + *model + "'";
-                break;
-        }
-    }
-
-    name += " (" + camera->id() + ")";
-
-    return name;
-}
 
 int main()
 {
     std::unique_ptr<CameraManager> cm = std::make_unique<CameraManager>();
     cm->start();
-
-    for (const auto& camera : cm->cameras())
-        std::cout << " - " << cameraName(camera.get()) << std::endl;
 
     if (cm->cameras().empty()) {
         std::cout << "No cameras were identified on the system" << std::endl;
@@ -51,9 +19,9 @@ int main()
         return EXIT_FAILURE;
     }
 
-    std::string cameraId = cm->cameras()[0]->id();
-    camera = cm->get(cameraId);
-    std::cout << "Cameras available yayy" << cameraName(camera.get()) << std::endl;
+    for (const auto& cam : cm->cameras())
+	std::cout << "Camera Found : " 
+		  << cam->id() << std::endl;
 
     return 0;
 }

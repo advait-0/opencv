796aaa2995d917cfedc3bf40201862303425bafd
Advait Dhamorikar <advaitdhamorikar@gmail.com>
2023-08-04 23:41:21 +0530

    Add all necessary IVideoCapture functions framework

--- modules/videoio/src/cap_libcamera.cpp
+++ modules/videoio/src/cap_libcamera.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>
 #include <memory>
-#include "cap_interface.hpp"
+#include <queue>
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
 #include <libcamera/libcamera.h>
@@ -33,12 +33,20 @@ public:
 
     bool grabFrame() CV_OVERRIDE;
 
-    bool retrieveFrame(int, OutputArray) CV_OVERRIDE
-    {
-        return false;
-    }
+    bool retrieveFrame(int, OutputArray) CV_OVERRIDE;
 
     virtual int getCaptureDomain() CV_OVERRIDE { return CAP_LIBCAMERA; }
+
+    ~CvCapture_libcamera_proxy()
+    {
+        if (opened_)
+        {
+            camera_->stop();
+            allocator.reset();
+            camera_->release();
+            cm_->stop();
+        }
+    }
 
     private:
     int ind;
@@ -47,22 +55,23 @@ public:
     static std::unique_ptr<CameraManager> cm_;
     static std::shared_ptr<Camera> camera_;
     static std::string cameraId_;
+    static std::queue<Request*> completedRequests_;
     bool opened_=false;
-    bool grabFrame_check;
-    bool grabbedinOpen;
     unsigned int allocated_;
     int reqlen_=0;
 
     protected:
     void cam_init();
     void cam_init(int index);
+    std::unique_ptr<Request> request;
     std::vector<std::unique_ptr<Request>> requests;
     std::unique_ptr<FrameBufferAllocator> allocator;
     static void processRequest(Request *request);
     static void requestComplete(Request *request);
-   
+
 };
 
+std::queue<Request*> CvCapture_libcamera_proxy::completedRequests_;
 std::unique_ptr<CameraConfiguration> CvCapture_libcamera_proxy::config_;
 std::unique_ptr<CameraManager> CvCapture_libcamera_proxy::cm_;
 std::string CvCapture_libcamera_proxy::cameraId_;
@@ -87,34 +96,39 @@ void CvCapture_libcamera_proxy::cam_init(int index)
 
 void CvCapture_libcamera_proxy::requestComplete(Request *request)
 {
-    std::cout<<"Entered requestComplete"<<std::endl;
+    if (request->status() == Request::RequestCancelled)
+    {
+		return;
+    }
+    else
+    {
+        std::cout<<"Request Not Cancelled"<<std::endl;
+    }
     processRequest(request);
-    std::cout<<"requestComplete Successful"<<std::endl;
 }
 
 void CvCapture_libcamera_proxy::processRequest(Request *request)
 {
     std::cout<<"Entered processRequest"<<std::endl;
 	std::cerr<< "Request completed: " << request->toString() << std::endl;
-    const Request::BufferMap &buffers = request->buffers();
-	request->reuse(Request::ReuseBuffers);
-	camera_->queueRequest(request);
-    std::cout<<"Reusing requests"<<std::endl;
+    completedRequests_.push(request);
 }
 
 bool CvCapture_libcamera_proxy::open(int index)
 {
     unsigned int nbuffers = UINT_MAX;
-    int ret=0;
+    int ret=0; 
     std::cerr<<"Entered Open";
     try
     {
         cameraId_ = cm_->cameras()[index]->id();
         camera_=cm_->get(cameraId_);
+        if (!camera_) 
+        { 
+            std::cerr << "Camera " << cameraId_ << " not found" << std::endl;
+        }
         camera_->acquire();
-
         config_ = camera_->generateConfiguration( { StreamRole::VideoRecording } );
-
         config_->validate();
         streamConfig_ = config_->at(index);
 	    std::cout << "Validated viewfinder configuration is: "<< streamConfig_.toString() << std::endl;
@@ -137,7 +151,7 @@ bool CvCapture_libcamera_proxy::open(int index)
         for (unsigned int i = 0; i < nbuffers; i++) 
         {
             std::cout<<"Creating Requests"<<std::endl;
-		    std::unique_ptr<Request> request = camera_->createRequest();
+		    request = camera_->createRequest();
 		    if (!request)
             {
                 std::cerr << "Can't create request" << std::endl;
@@ -161,6 +175,15 @@ bool CvCapture_libcamera_proxy::open(int index)
             reqlen_ = requests.size();
             std::cout<<"internal request size: "<<requests.size()<<std::endl;
         }
+
+        
+        camera_->requestCompleted.connect(requestComplete);
+        camera_->start();
+        for (std::unique_ptr<Request> &request : requests)
+        {
+         	camera_->queueRequest(request.get());
+            std::cout<<"Request getting queued"<<std::endl;
+        }
         opened_ = true;
         // return true;
     }
@@ -168,7 +191,7 @@ bool CvCapture_libcamera_proxy::open(int index)
     catch(const std::exception& e)
     {
         std::cerr << e.what() << '\n';
-        std::cout<<"CvCapture_libcamera_proxy failed";
+        std::cout<<"CvCapture_libcamera_proxy::open failed";
         opened_=false;
     }
     return opened_;
@@ -176,31 +199,38 @@ bool CvCapture_libcamera_proxy::open(int index)
 
 bool CvCapture_libcamera_proxy::grabFrame()
 {
+    std::cout<<opened_<<std::endl;
     if(opened_==false)
     {
         open(0);
     }
-
-    camera_->requestCompleted.connect(requestComplete);
-    camera_->start();
-    for (std::unique_ptr<Request> &request : requests)
-    {
-     	camera_->queueRequest(request.get());
-        std::cout<<"Request getting queued"<<std::endl;
-    }
-
-    camera_->stop();
-	allocator.reset();
-	camera_->release();
-	camera_.reset();
-	cm_->stop();
  
     return true;
 }
-// bool CvCapture_libcamera_proxy::grabFrame()
-// {
 
-// }
+bool CvCapture_libcamera_proxy::retrieveFrame(int, OutputArray)
+{
+    if(completedRequests_.empty())
+    {
+        std::cout<<"completedRequests is empty"<<std::endl;
+        // return false;
+    }
+    std::cout<<"retrieveFrame"<<std::endl;
+    std::cout<<"Retrieved Frame "<<completedRequests_.front()->sequence()<<std::endl;
+    auto nextProcessedRequest = completedRequests_.front();
+    completedRequests_.pop();
+    
+    //var stores the first value of the queue
+    //Mat handling to be done here
+
+    std::cout<<"Reusing buffer in processRequest"<<std::endl;
+    nextProcessedRequest->reuse(Request::ReuseBuffers);
+    std::cout<<"retrieveFrame Request Queued"<<std::endl;
+	camera_->queueRequest(nextProcessedRequest);
+ 
+
+    return true;
+}
 
 cv::Ptr<cv::IVideoCapture> create_libcamera_capture_cam(int index)
 {
